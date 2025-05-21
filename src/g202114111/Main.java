af77eca0d409449c90c598fa7155a99bb26fec91
kangcheolung <skhu202114111@gmail.com>
2025-05-21 09:25:56 +0900

    five

--- src/g202114111/Main.java
+++ src/g202114111/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("강철웅");
         World.run();
+        World.run2();
     }
 }
--- src/g202114111/World.java
+++ src/g202114111/World.java
@@ -1,5 +1,9 @@
 public class World {
     public static void run() {
-        System.out.println("202114111");  
+        System.out.println("202114111");
+    }
+
+    public static void run2() {
+        System.out.println("hello world");
     }
 }
